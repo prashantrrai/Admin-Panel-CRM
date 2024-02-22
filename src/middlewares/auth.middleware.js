2ce1daae527d9c0c59cd3aa483eb843fd7733e82
Prashant Rai <19ce073@gardividyapith.ac.in>
2024-02-22 10:36:11 +0530

    --BUG Fixed Authorization Token Split issue.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,20 +3,25 @@ const jwt = require('jsonwebtoken');
 
 const secretKey = process.env.SECRET_KEY;
 
-
 exports.authenticateToken = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
+    try {
+        const token = req.headers.authorization.split(' ')[1];
 
-    if (!token) {
-        return res.status(401).json({ message: 'Authorization token is required.' });
-    }
-
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) {
-            console.error("TOKEN ERROR :", err)
-            return res.status(403).json({ message: 'Invalid or expired token.' });
+        if (!token) {
+            return res.status(401).json({ message: 'Authorization token is required.' });
         }
-        req.user = decoded;
-        next();
-    });
+
+        jwt.verify(token, secretKey, (err, decoded) => {
+            if (err) {
+                console.error("TOKEN ERROR :", err)
+                return res.status(403).json({ message: 'Invalid or expired token.' });
+            }
+            req.user = decoded;
+            next();
+        });
+    } catch (error) {
+        console.error("HEADER ERROR :", error);
+        return res.status(401).json({ message: 'Authorization header missing or invalid.' });
+    }
 };
+
